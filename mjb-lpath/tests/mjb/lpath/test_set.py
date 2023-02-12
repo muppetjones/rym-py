06405cc9852d30eec0c39fddf62b5094bf9e64db
Stephen Bush <muppetjones@gmail.com>
2023-02-11 21:51:04 -0600

    added lpath.set

--- mjb-lpath/tests/mjb/lpath/test_set.py
+++ mjb-lpath/tests/mjb/lpath/test_set.py
@@ -25,6 +25,7 @@ class TestSet(ThisTestCase):
             "b": ["b0", "b1", "baz"],
             "c": {"foo": "meh"},
             "d": SimpleNamespace(bar="d"),
+            "e": (0, 1, 2),
         }
         return {
             "list": list(data.values()),
@@ -34,17 +35,26 @@ class TestSet(ThisTestCase):
 
     def test_raises_if_key_is_not_a_str(self):
         example = self.get_example()
-        with self.assertRaisesRegex(TypeError, "invalid"):
-            MOD.set(example, ["list.0", "mapping.a"])
+        with self.assertRaisesRegex(TypeError, "must be a string"):
+            MOD.set(example, ["list.0", "mapping.a"], "str required")
 
-    def test_raises_if_key_parent_does_not_exist(self):
+    def test_raises_if_target_is_immutable(self):
+        example = self.get_example()
+        with self.assertRaisesRegex(TypeError, "not support item assignment"):
+            MOD.set(example, "mapping.e.0", "err")
+
+        with self.assertRaisesRegex(TypeError, "not support item assignment"):
+            MOD.set(example, "mapping.c.foo.0", "err")
+
+    def test_raises_if_key_parent_does_not_exist_or_invalid(self):
         example = self.get_example()
         tests = [
             # (exc_type, value, path)
-            (IndexError, None, "list.4"),
+            (IndexError, None, "list.42"),
             (IndexError, None, "list.1.4"),
-            (IndexError, None, "list.d.hehe"),
-            (KeyError, None, "mapping.e.missing.nested"),
+            (AttributeError, None, "list.3.hehe"),
+            (ValueError, None, "mapping.e.missing.nested"),
+            (KeyError, None, "mapping.f.missing.nested"),
             (AttributeError, None, "mapping.d.foo"),
             (AttributeError, None, "namespace.f"),
         ]
@@ -55,31 +65,40 @@ class TestSet(ThisTestCase):
 
     def test_sets_value_for_given_path(self):
         example = self.get_example()
+        value = 42
         tests = [
-            # (value, path)
+            # (key, lookup lambda)
+            ("other", lambda x: x["other"]),
+            ("list.0", lambda x: x["list"][0]),
+            ("list.1.1", lambda x: x["list"][1][1]),
+            ("list.2.foo", lambda x: x["list"][2]["foo"]),
+            ("list.3.bar", lambda x: x["list"][3].bar),
+            ("mapping.b.2", lambda x: x["mapping"]["b"][2]),
+            ("namespace.b", lambda x: x["namespace"].b),
         ]
-        for value, path in tests:
+        for path, retrieve in tests:
             instance = copy.deepcopy(example)
             with self.subTest(value=value, path=path):
                 MOD.set(instance, path, value)
-                found = MOD.get(instance, path)
+                found = retrieve(instance)
                 expected = value
                 self.assertEqual(expected, found)
 
     def test_adds_item_if_parent_is_mapping(self):
         example = self.get_example()
+        value = 42
         tests = [
-            # (value, path)
-            (0, "list.c.missing"),
-            (1, "mapping.e"),
-            (2, "mapping.c.missing"),
-            (3, "namespace.c.missing"),
+            # (path, lookup lambda)
+            ("list.2.b", lambda x: x["list"][2]["b"]),
+            ("mapping.f", lambda x: x["mapping"]["f"]),
+            ("mapping.c.u", lambda x: x["mapping"]["c"]["u"]),
+            ("namespace.c.u", lambda x: x["namespace"].c["u"]),
         ]
-        for value, path in tests:
+        for path, retrieve in tests:
             instance = copy.deepcopy(example)
             with self.subTest(value=value, path=path):
                 MOD.set(instance, path, value)
-                found = MOD.get(instance, path)
+                found = retrieve(instance)
                 expected = value
                 self.assertEqual(expected, found)
 
@@ -96,9 +115,8 @@ class TestSetWithDelim(ThisTestCase):
     def test_honors_given_delim(self):
         example = self.get_example()
         key = "foo/bar"
-        value = 42
-
-        MOD.get(example, key, value, delim="/")
+        value = (0, 1, 42)
+        MOD.set(example, key, value, delim="/")
 
         found = example["foo"]["bar"]
         expected = value
@@ -110,8 +128,11 @@ class TestSetWithDelim(ThisTestCase):
 
         example = self.get_example()
         key = "foo@bar"
-        expected = example["foo"]["bar"]
-        found = MOD.get(example, key)
+        value = {"a": "mapping"}
+        MOD.set(example, key, value)
+
+        found = example["foo"]["bar"]
+        expected = value
         self.assertEqual(expected, found)
 
 
